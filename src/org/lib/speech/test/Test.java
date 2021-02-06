31d602b9f9d459b6320f9a30d151c4283786d5ec
linlsyf <linlsyf@gmail.com>
2021-02-06 14:57:46 +0800

    实现可以播放

--- src/org/lib/speech/test/Test.java
+++ src/org/lib/speech/test/Test.java
@@ -1,7 +1,16 @@
 package org.lib.speech.test;
 
+import org.lib.speech.engine.Engine;
+import org.lib.speech.engine.SpeechEngine;
+import org.lib.speech.process.DefaultStreamProcess;
+import org.lib.speech.process.ProcessCenter;
+
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Iterator;
+import java.util.List;
 
 public class Test {
 
@@ -37,29 +46,30 @@ public class Test {
 //        }
 //    }
 
-    public static void main(String[] args) {
-        String localhost_dir = "";
-        Runtime run = null;
-        try {
-            run = Runtime.getRuntime();
-            //调用解码器来将wav文件转换为mp3文件
-            Process p=run.exec("/usr/bin/lame /java/5/a.wav"); //16为码率，可自行修改
-
-            //释放进程
-            p.getOutputStream().close();
-            p.getInputStream().close();
-            p.getErrorStream().close();
-            p.waitFor();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }finally{
-            //最后都要执行的语句
-            //run调用lame解码器最后释放内存
-            run.freeMemory();
-        }
-
-    }
+//    public static void main(String[] args) {
+//        String localhost_dir = "";
+//        Runtime run = null;
+//        try {
+//            run = Runtime.getRuntime();
+//            //调用解码器来将wav文件转换为mp3文件
+//            Process p=run.exec("/usr/bin/lame /java/5/a.wav"); //16为码率，可自行修改
+//
+//
+//            //释放进程
+//            p.getOutputStream().close();
+//            p.getInputStream().close();
+//            p.getErrorStream().close();
+//            p.waitFor();
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }finally{
+//            //最后都要执行的语句
+//            //run调用lame解码器最后释放内存
+//            run.freeMemory();
+//        }
+//
+//    }
 //    public static void main(String[] args) {
 //        String localhost_dir = "";
 //        Runtime run = null;
@@ -83,6 +93,54 @@ public class Test {
 //        }
 //
 //    }
+
+
+    public static void main(String[] args) {
+        // data    properties 要放到D盘后续需要修改
+// 建造一个流处理器，参数设置是否重新读取字典文件
+        ProcessCenter pc = new DefaultStreamProcess(true);
+
+// 建立一个语音引擎，第二个参数设置是否转换为粤语口语发音
+        Engine engine = new SpeechEngine(pc, true);
+
+// 任何一个String作为你想要它发音的句子
+        String sentences = "你可以在这里尝试任何一个句子，看看它是如何发音的";
+
+// 第一种方法：直接要它发音
+        engine.getPronounces(sentences);
+
+// 第二种方法：句子在一个txt文档中，你要它把txt中的内容读出来，第二个参数设置是否将文档内容输出到控制台显示
+//        try {
+//            engine.getPronounces(new File("C:/a.txt"), false);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//// 第三种方法：把发音保存在一个.au的声音文件中，目前只支持保存到这种文件，当然你也可以自己扩展
+//        try {
+//            engine.getPronouncesFile(sentences, new File("C:/a.au"));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+// 另外，如果你想获得初始的发音素材，可以这样显示到控制台
+        List<Object[]> list = engine.getPronounceElements(sentences);
+        Iterator<Object[]> iter = list.iterator();
+        while (iter.hasNext()) {
+            Object[] obj = iter.next();
+            if (obj[0] instanceof File) {
+                for (int i = 0; i < obj.length; i++) {
+                    File file = (File) obj[i];
+                    System.out.print(file.getName() + " ");
+                }
+            } else {
+                for (int i = 0; i < obj.length; i++) {
+                    System.out.print(obj[i] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
 
 }
 
